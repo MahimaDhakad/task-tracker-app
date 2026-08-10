d8cf0544195adb7c221421b933b091a16bd069b2
Mahima Dhakad <mahimadhakad137@gmail.com>
2026-08-10 14:45:19 +0530

    Fixed production URLs

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,8 +4,7 @@
 import { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'https://task-tracker-app-1-l9eu.onrender.com/api/auth';
-
+const API_URL = "https://task-tracker-app-1-l9eu.onrender.com/api/auth";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
